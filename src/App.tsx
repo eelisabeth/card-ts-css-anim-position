5832934dc34235816954b3ff75888ee1b38923a2
eelisabeth <eelisabeth.pro@gmail.com>
2022-05-30 01:10:28 +0200

    Card #2 - create a simple card component JSX structure & add base style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Card, { CardClass } from './Card/Card';
 import ProductCard from './productCard/ProductCard';
 
 function App() {
@@ -12,6 +13,13 @@ function App() {
         price={20}
         imageSrc={'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80'}
         imageDesc={''}
+      />
+
+      <Card
+        title='Blue Pop'
+        subtext='This is a nice text that you can show'
+        className={CardClass.BluePop}
+        imageUrl={'https://images.unsplash.com/photo-1535957998253-26ae1ef29506?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29ya3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'}
       />
     </div>
   );
---
+++ src/Card/Card.tsx
@@ -0,0 +1,33 @@
+export enum CardClass {
+    BluePop ='blue-pop',
+    GreenFrame ='green-frame',
+    WarmFilter ='warm-filter'
+}
+
+interface CardProps {
+    title: string;
+    subtext: string;
+    className: CardClass;
+    imageUrl: string;
+}
+
+const Card: React.FC<CardProps> = ({
+    title,
+    subtext,
+    className,
+    imageUrl,
+}) => {
+    return (
+        <figure className={className.toString()}>
+            <img src={imageUrl} alt="background" />
+            <figcaption>
+                <div>
+                    <h2>{title}</h2>
+                    <p>{subtext}</p>
+                </div>
+            </figcaption>
+        </figure>
+    )
+}
+
+export default Card
